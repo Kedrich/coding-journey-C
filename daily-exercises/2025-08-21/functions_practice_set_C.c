53703c9f4043344f6bb9ee52896c906772c6c97e
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-08-21 11:00:04 +0800

    Updated

--- daily-exercises/2025-08-21/functions_practice_set_C.c
+++ daily-exercises/2025-08-21/functions_practice_set_C.c
@@ -16,9 +16,15 @@ int divide_and_store (int *first, int *second)
 int main (void)
 {
 
-    int first = 17;
-    int second = 5;
+    int first;
+    int second;
     int original;
+
+    printf("Please enter your first number: ");
+    scanf("%d", &first);
+    printf("Please enter your second number: ");
+    scanf("%d", &second);
+
     original = divide_and_store(&first, &second);
     printf("Original: %d\nQoutient: %d\nRemainder: %d", original, first, second);
     return 0;
