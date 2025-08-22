386b3c136ab9753db7d0c1927db6f3fbdd2d7f19
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-08-22 17:27:38 +0800

    Hello

---
+++ daily-exercises/2025-08-22/randomNumbers.c
@@ -0,0 +1,50 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#include <time.h>
+
+int randomNum (int min, int max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
+
+int main(void)
+{
+    srand(time(NULL));
+    
+    int guess;
+    int count = 0;
+    int result;
+
+    result = randomNum(1, 100);
+
+    printf("*** NUMBER GUESSING GAME! ***\n");
+    printf("I'm thinking of a number between 1 and 100!\n\n");
+
+    printf("Please enter your guess number: ");
+    scanf("%d", &guess);
+    count++; // Count the first guess
+    
+    while(guess != result)
+    {
+        if (guess < result)
+        {
+            printf("Too low! Try again.\n");
+        }
+        else if (guess > result)
+        {
+            printf("Too high! Try again.\n");
+        }
+        // Note: We don't need the else if (result == guess) here 
+        // because the while condition already checks this
+
+        printf("Please enter your guess number: ");
+        scanf("%d", &guess);
+        count++; // Count each guess
+    }
+    
+    // This runs when guess == result (loop exits)
+    printf("That's correct! The number was %d\n", result);
+    printf("Congratulations! You won the Guessing Game in %d attempts!\n", count);
+
+    return 0;
+}
