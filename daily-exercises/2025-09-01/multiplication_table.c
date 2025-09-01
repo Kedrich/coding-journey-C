ab719886db0c537c6185c113eb4ba1d6bf252c3e
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-09-01 10:04:44 +0800

    I did an another code called Multiplication Table

---
+++ daily-exercises/2025-09-01/multiplication_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int number, product, count;
+
+    printf("Please entere a number: ");
+    scanf("%d", &number);
+
+    for(count = 1; count <= 10; count++)
+    {
+        product = count * number;
+        printf("%d x %d = %d\n", number, count, product);
+    }
+
+
+    return 0;
+}
