f08b50fc9bb0fc2fa3b1461b06e288742441e5f9
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-09-07 16:57:05 +0800

    I did a practice problem array part 1

--- daily-exercises/2025-09-07/main.c
+++ daily-exercises/2025-09-07/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
+void countEvenorOddNumbers(int arr[], int n)
+{
+    int odd = 0; 
+    int even = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    printf("\nOdd: %d, Even: %d", even, odd);
+
+}
+
 int main(void)
 {
-    printf("Hello World!");
+    int n;  
+    printf("Please enter the number of arrays: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        printf("Please enter the %d element: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Arrays: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    countEvenorOddNumbers(arr, n);
+
+    return 0;
 }
