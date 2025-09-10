541d0334db7e25a0313b4d20396140c622cf2fb6
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-09-10 15:55:06 +0800

    I did an another school exercise

--- daily-exercises/2025-09-10/exercise1.c
+++ daily-exercises/2025-09-10/exercise1.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+
+int secondtohoursConverter(int inputSeconds)
+{
+    int hours = inputSeconds / 3600;
+    return hours;
+}
+
+int secondtominutesConverter(int inputSeconds)
+{
+    int minutes = (inputSeconds % 3600) / 60;
+}
+
+int remainingSeconds(int inputSeconds, int hours, int minutes)
+{
+    int seconds = inputSeconds - ((hours * 3600) + (minutes * 60)) ;
+}
+
+int main(void)
+{
+
+    int inputSeconds;
+    printf("Enter total of seconds: ");
+    scanf("%d", &inputSeconds);
+
+    int hours = secondtohoursConverter(inputSeconds);
+    int minutes = secondtominutesConverter(inputSeconds);
+    int seconds = remainingSeconds(inputSeconds, hours, minutes);
+
+    printf("%d hours, %d minutes, %d seconds.", hours, minutes, seconds);
+
+
+
+    return 0;
+}
