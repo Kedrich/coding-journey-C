b570f16e4fc7e4dc20e57c4cafecab0e18517adf
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-09-10 16:06:29 +0800

    Changed the name

--- daily-exercises/2025-09-10/CUSTODIO-KIMEDRICH_PE1_problem1.c
+++ daily-exercises/2025-09-10/CUSTODIO-KIMEDRICH_PE1_problem1.c
@@ -9,11 +9,13 @@ int secondtohoursConverter(int inputSeconds)
 int secondtominutesConverter(int inputSeconds)
 {
     int minutes = (inputSeconds % 3600) / 60;
+    return minutes;
 }
 
 int remainingSeconds(int inputSeconds, int hours, int minutes)
 {
-    int seconds = inputSeconds - ((hours * 3600) + (minutes * 60)) ;
+    int seconds = inputSeconds - ((hours * 3600) + (minutes * 60));
+    return seconds;
 }
 
 int main(void)
@@ -27,9 +29,7 @@ int main(void)
     int minutes = secondtominutesConverter(inputSeconds);
     int seconds = remainingSeconds(inputSeconds, hours, minutes);
 
-    printf("%d hours, %d minutes, %d seconds.", hours, minutes, seconds);
-
-
+    printf("%d hour/s, %d minute/s, %d second/s.", hours, minutes, seconds);
 
     return 0;
 }
