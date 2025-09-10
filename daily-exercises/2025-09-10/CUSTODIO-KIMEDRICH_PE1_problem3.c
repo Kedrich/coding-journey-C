c8eea51e90f6371e81fec5b6f2d6d130a4de5e68
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-09-10 16:22:15 +0800

    I did an another exercise in our school

--- daily-exercises/2025-09-10/CUSTODIO-KIMEDRICH_PE1_problem3.c
+++ daily-exercises/2025-09-10/CUSTODIO-KIMEDRICH_PE1_problem3.c
@@ -1,3 +1,36 @@
 #include <stdio.h>
 
-int main    
+float totalbillCalculation(float amount, float tip)
+{
+    float total_bill = amount + (amount / tip);
+    return total_bill;
+}
+
+float paymentPerPerson(float total_bill, float people)
+{
+    float payment_per_person = total_bill / people;
+    return payment_per_person;
+}
+
+int main()
+{
+
+    float amount, tip, people;
+
+    printf("Enter the bill amount: $");
+    scanf("%f", &amount);
+    printf("Enter the tip percentage: ");
+    scanf("%f", &tip);
+    printf("Enter the number of people/s: ");
+    scanf("%f", &people);
+
+    float total_bill = totalbillCalculation(amount, tip);
+    float payment_per_person = paymentPerPerson(total_bill, people);
+
+    printf("Total bill including tip: $%.2f\n", total_bill);
+    printf("Each person should pay: $%.2f", payment_per_person);
+    
+    
+
+    return 0;
+}
