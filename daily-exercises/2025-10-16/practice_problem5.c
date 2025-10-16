491c21f25680cd94185f5d5200e147723a0a23ae
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-16 14:10:54 +0800

    Practice Problem 5

---
+++ daily-exercises/2025-10-16/practice_problem5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int basic_salary, deduction_percent;
+
+    printf("Enter basic salary: ");
+    scanf("%d", &basic_salary);
+
+    printf("Enter deduction percent: ");
+    scanf("%d", &deduction_percent);
+
+    float deduction_salary_amount = (basic_salary * (deduction_percent / 100.00));
+
+    float net_salary = basic_salary - deduction_salary_amount;
+
+    printf("Net Salary: %.2f", net_salary);
+    
+
+    return 0;
+}
