e25e3505f95b2fafc38c4068d7def268cf4b3868
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-17 17:20:43 +0800

    Pointer Array

---
+++ daily-exercises/2025-10-17/pointerarray.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int myNumbers[4] = {25, 50, 75, 100};
+    int *ptr = myNumbers;
+    
+    printf("\n");
+
+    for (int i = 0; i < 4; i++) 
+    {
+       printf("%d\n", *(ptr+i));
+    }
+
+    return 0;
+}
