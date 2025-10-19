7c5c38492c397f634efc466809becb8c3aae5178
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-19 16:26:30 +0800

    Practice

--- daily-exercises/2025-10-19/practice.c
+++ daily-exercises/2025-10-19/practice.c
@@ -4,13 +4,15 @@
 int main(void)
 {
     
-    int i = 1;
+    int a[] = {1,2,3,4,5,6};
+    int *p = &a[3];
+    int *q = &a[5];
+    printf("%d\n", p);
+    printf("%d\n", q);
+    printf("%d\n", p <= q);
+    printf("%d\n", p >= q);
+    q = &a[3];
+    printf("%d", p==q);
     
-    int *p;
-    p = &i;
-
-    printf("%d ", *p);
-    printf("%d", *(&i));
-
     return 0;
 }
