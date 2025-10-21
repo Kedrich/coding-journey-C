a31296a5ddca61e9bc27c80dfbed4e9140e7915e
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-21 11:20:23 +0800

    adjusted

--- Exercises C Programming - A Modern Approach/section2two.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Parkinson's Law: \nWorks expands so as to ");
-    printf("fill the time\n");
-    printf("available for its completion.\n");
-    return 0;
-}
