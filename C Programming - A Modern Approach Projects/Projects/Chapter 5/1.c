b5392def4245634429f2163efbf3745bca442823
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-22 19:00:59 +0800

    Project 2

--- C Programming - A Modern Approach Projects/Projects/Chapter 5/1.c
+++ C Programming - A Modern Approach Projects/Projects/Chapter 5/1.c
@@ -3,7 +3,42 @@
 int main(void)
 {
 
-    
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
 
+    if(0 <= number && number <= 9)
+    {
+        printf("The number %d has 1 digit", number);
+    }
+    else if (10 <= number && number <= 99)
+    {
+        printf("The number %d has 2 digits", number);
+    }
+    else if (100 <= number && number <= 999)
+    {
+        printf("The number %d has 3 digits", number);
+    }
+    else if (1000 <= number && number <= 9999)
+    {
+        printf("The number %d has 4 digits", number);
+    }
+    else if (10000 <= number && number <= 99999)
+    {
+        printf("The number %d has 5 digits", number);
+    }
+    else if (100000 <= number && number <= 999999)
+    {
+        printf("The number %d has 6 digits", number);
+    }
+    else if (number >= 1000000)
+    {
+        printf("The number %d has more than 6 digits", number);
+    }
+    else
+    {
+        printf("Please enter positive number only.");
+    }
+    
     return 0;
 }
---
+++ C Programming - A Modern Approach Projects/Projects/Chapter 5/2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int hour, minute;
+    printf("Enter a 24-hour time: ");
+    scanf("%d:%d", &hour, &minute);
+
+    printf("Equivalent 12-hour time: ");
+
+    switch (hour)
+    {
+    case 0: printf("12"); break;  // Renamed 00 to 0
+    case 1: printf("1"); break;
+    case 2: printf("2"); break;
+    case 3: printf("3"); break;
+    case 4: printf("4"); break;
+    case 5: printf("5"); break;
+    case 6: printf("6"); break;
+    case 7: printf("7"); break;
+    case 8: printf("8"); break;
+    case 9: printf("9"); break;
+    case 10: printf("10"); break;
+    case 11: printf("11"); break;
+    case 12: printf("12"); break;
+    case 13: printf("1"); break;
+    case 14: printf("2"); break;
+    case 15: printf("3"); break;
+    case 16: printf("4"); break;
+    case 17: printf("5"); break;
+    case 18: printf("6"); break;
+    case 19: printf("7"); break;
+    case 20: printf("8"); break;
+    case 21: printf("9"); break;
+    case 22: printf("10"); break;
+    case 23: printf("11"); break;
+    case 24: printf("12"); break;
+    }
+
+    printf(":%02d ", minute); 
+
+    if (hour == 0 || hour == 24 || (hour >= 1 && hour <= 11))
+    {
+        printf("AM");
+    }
+    else
+    {
+        printf("PM");
+    }
+
+    return 0;
+}
