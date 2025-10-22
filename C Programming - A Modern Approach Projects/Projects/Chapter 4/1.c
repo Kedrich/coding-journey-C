a4eeb6e0e5bc86131fc25bf90e52d1f38ce5cdf7
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-22 16:24:14 +0800

    Project 1

---
+++ C Programming - A Modern Approach Projects/Projects/Chapter 4/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("Enter a two-digit number: ");
+    scanf("%d", &n);
+
+    int reversed = (n % 10) * 10 + (n / 10);
+
+    printf("The reversal is: %d\n", reversed);
+
+    return 0;
+}
