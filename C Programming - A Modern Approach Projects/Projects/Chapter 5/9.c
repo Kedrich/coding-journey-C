790293618c623fffc34001110341ed5cd380df13
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-23 10:41:57 +0800

    Project 9

---
+++ C Programming - A Modern Approach Projects/Projects/Chapter 5/9.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int month1, day1, year1;
+    int month2, day2, year2;
+
+    printf("Enter first date (mm/dd/yy): ");
+    scanf("%d/%d/%d", &month1, &day1, &year1);
+
+    printf("Enter second date (mm/dd/yy): ");
+    scanf("%d/%d/%d", &month2, &day2, &year2);
+
+    if (year1 < year2 ||
+       (year1 == year2 && month1 < month2) ||
+       (year1 == year2 && month1 == month2 && day1 < day2))
+    {
+        printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
+               month1, day1, year1, month2, day2, year2);
+    }
+    else if (year1 == year2 && month1 == month2 && day1 == day2)
+    {
+        printf("The dates are the same.\n");
+    }
+    else
+    {
+        printf("%d/%d/%02d is earlier than %d/%d/%02d\n",
+               month2, day2, year2, month1, day1, year1);
+    }
+
+    return 0;
+}
