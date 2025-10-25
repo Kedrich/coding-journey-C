a5039ab7661933b9ed52a980a1e54b54f77e8cd3
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-26 00:07:14 +0800

    Project 3

--- C Programming - A Modern Approach Projects/Projects/Chapter 6/3.c
+++ C Programming - A Modern Approach Projects/Projects/Chapter 6/3.c
@@ -3,11 +3,30 @@
 int main(void)
 {
     
-    int e,d;
+    int e,d, remainder;
     printf("Enter a fraction: ");
     scanf("%d/%d", &e, &d);
 
-    
+    int gcde = e, gcdd = d;
+
+    if(d == 0)
+    {
+        printf("Undefined");
+        return 1;
+    }
+
+    while(gcde != 0)
+    {   
+        remainder = gcdd % gcde;
+        gcdd = gcde;
+        gcde = remainder;
+    }
+
+    e = e / gcdd;
+    d = d / gcdd;
+
+    printf("In lowest term: %d/%d", e, d);
+
 
     return 0;
 }
