fd6f07f9355cdfc4217a01c5ae8d9bb18b43084b
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-28 18:12:39 +0800

    exercise9

---
+++ daily-exercises/2025-10-28/exercise9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int r;
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+
+    for(int i = 1; i <= r; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
