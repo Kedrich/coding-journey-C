ba021dc28e1eb9c292f8df9fc73196e308017b81
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-31 07:48:05 +0800

    1

---
+++ C Programming - A Modern Approach Projects/Exercises/exe.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int i, n = 2;
+    
+    for (i = 1; i <= 20; i++)
+    {
+        
+        if(i == n)
+        {
+            n += 2;
+            continue;
+        }
+
+        printf("%d ", i);
+
+    }
+    
+
+    return 0;
+}
