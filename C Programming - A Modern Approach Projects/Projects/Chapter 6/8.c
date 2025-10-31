2ff434922a2ba74164d28ad7c50026c72b58ee70
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-31 09:07:16 +0800

    8

--- C Programming - A Modern Approach Projects/Projects/Chapter 6/8.c
+++ C Programming - A Modern Approach Projects/Projects/Chapter 6/8.c
@@ -4,18 +4,35 @@ int main(void)
 {
 
     int days, starting_day;
+    int i, day;
     printf("Enter number of days in month: ");
     scanf("%d", &days);
     printf("Enter starting day of the week: (1=Sun, 7=Sat): ");
     scanf("%d", &starting_day);
 
-    for(int i = 1; i <= n; i++)
+    if (starting_day < 1)
+        starting_day = 1;
+    else if (starting_day > 7)
+        starting_day = 7;
+
+    for(i = 1; i < starting_day; i++)
     {
-        if (i)
+        printf("   ");
+    }
+
+    for(day = 1; day <= days; day++)
+    {
+        printf("%3d", day);
+
+        if((day + starting_day - 1) % 7 == 0)
         {
-            
+            printf("\n");
         }
-        
+    }
+
+    if ((days + starting_day - 1) % 7 != 0)
+    {
+        printf("\n");
     }
 
     return 0;
