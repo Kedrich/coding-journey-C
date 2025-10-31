5f45e64bf5f12d0007b4b1da7d678be45814cddb
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-10-31 08:51:10 +0800

    5

--- C Programming - A Modern Approach Projects/Projects/Chapter 6/5.c
+++ C Programming - A Modern Approach Projects/Projects/Chapter 6/5.c
@@ -2,8 +2,18 @@
 
 int main(void)
 {
+    int n, reversed = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-    
+    do
+    {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;          
+    } while (n > 0);
+
+    printf("The reversal is: %d\n", reversed);
 
     return 0;
 }
