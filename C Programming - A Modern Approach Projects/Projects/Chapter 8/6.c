5bac743ba9a8bf6713b04414f14c8c1e49f2443c
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-01 13:53:43 +0800

    6

--- C Programming - A Modern Approach Projects/Projects/Chapter 8/6.c
+++ C Programming - A Modern Approach Projects/Projects/Chapter 8/6.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main(void)
-{
+#define MAX_MSG 100 
 
-    
+int main(void) {
+    char message[MAX_MSG];
+    char ch;
+    int i = 0;
 
+    printf("Enter message: ");
+
+    while ((ch = getchar()) != '\n' && i < MAX_MSG) {
+        message[i] = ch;
+        i++;
+    }
+
+    printf("In B1FF-speak: ");
+
+    for (int j = 0; j < i; j++) 
+    {
+
+        ch = toupper(message[j]);
+
+        switch (ch) {
+            case 'A':
+                printf("4");
+                break;
+            case 'B':
+                printf("8");
+                break;
+            case 'E':
+                printf("3");
+                break;
+            case 'I':
+                printf("1");
+                break;
+            case 'O':
+                printf("0");
+                break;
+            case 'S':
+                printf("5");
+                break;
+            default:
+                printf("%c", ch);
+                break;
+        }
+    }
+
+    for (int j = 0; j < 10; j++) {
+        printf("!");
+    }
+    printf("\n");
 
     return 0;
 }
