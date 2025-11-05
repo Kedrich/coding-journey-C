fe007cfd2bd32a4de2e49372d1de782dc981fed6
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-05 17:24:34 +0800

    Problem 4

---
+++ daily-exercises/2025-11-5/CUSTODIO-KIMEDRICH_PE4_problem4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name[99];
+    float weight, height, bmi;
+
+    printf("Enter name: ");
+    scanf(" %s", name);
+    printf("Enter weight (kg): ");
+    scanf("%f", &weight);
+    printf("Enter height (m): ");
+    scanf("%f", &height);
+
+    if (weight <= 0 || height <= 0)
+    {
+        printf("Please enter valid positive values for weight and height.\n");
+        return 0;
+    }
+
+    bmi = weight / (height * height);
+
+    printf("%s's BMI: %.2f\n\n", name, bmi);
+
+    if (bmi < 18.5)
+    {
+        printf("Category: Underweight\n");
+    }
+    else if (bmi < 25)
+    {
+        printf("Category: Normal weight\n");
+    }
+    else if (bmi < 30)
+    {
+        printf("Category: Overweight\n");
+    }
+    else
+    {
+        printf("Category: Obese\n");
+    }
+
+    return 0;
+}
