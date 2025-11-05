c41a8ddcb11091b42e4b2c6f9e3f2890eadb3ca9
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-05 15:20:57 +0800

    Selection 16

--- daily-exercises/2025-11-5/selection16.c
+++ daily-exercises/2025-11-5/selection16.c
@@ -3,7 +3,40 @@
 int main(void)
 {
 
-    int n;
+    int grade;
+    printf("Enter your grade: ");
+    scanf("%d", &grade);
+
+    if (grade < 0)
+    {
+        printf("Please enter a valid grade.");
+        return 0;
+    }
+
+    if (grade >= 90 && grade <= 100)
+    {
+        printf("Your grade is %d and your rating is A", grade);
+    }
+    else if (grade >= 80 && grade <= 89)
+    {
+        printf("Your grade is %d and your rating is B", grade);
+    }
+    else if (grade >= 70 && grade <= 79)
+    {
+        printf("Your grade is %d and your rating is C", grade);
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        printf("Your grade is %d and your rating is D", grade);
+    }
+    else if (grade < 60)
+    {
+        printf("Your rating is F", grade);
+    }
+    else
+    {
+        printf("Please enter a valid grade.");
+    }
 
     return 0;
 }
