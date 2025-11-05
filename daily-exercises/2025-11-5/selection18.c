9bc9c73e78b8901e88cc6b70b9b9f349901b7e31
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-05 16:03:47 +0800

    Selection 18

--- daily-exercises/2025-11-5/selection18.c
+++ daily-exercises/2025-11-5/selection18.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-
     float weight, height;
 
     printf("Enter weight (in kg): ");
@@ -10,24 +9,31 @@ int main(void)
     printf("Enter height (in cm): ");
     scanf("%f", &height);
 
+    if (weight <= 0 || height <= 0)
+    {
+        printf("Please enter valid positive values for weight and height.\n");
+        return 0;
+    }
+
     height = height * 0.01;
+
     float bmi = weight / (height * height);
 
     if (bmi < 18.5)
     {
-        printf("Your BMI is %.2f and you are underweight.", bmi);
+        printf("Your BMI is %.2f and you are underweight.\n", bmi);
     }
-    else if (bmi >= 18.5 && bmi <= 24.9)
+    else if (bmi < 25)
     {
-        printf("Your BMI is %.2f and you are normal.", bmi);
+        printf("Your BMI is %.2f and you are normal.\n", bmi);
     }
-    else if (bmi >= 25 && bmi <= 29.9)
+    else if (bmi < 30)
     {
-        printf("Your BMI is %.2f and you are overweight.", bmi);
+        printf("Your BMI is %.2f and you are overweight.\n", bmi);
     }
-    else if (bmi >= 30)
+    else
     {
-        printf("Your BMI is %.2f and you are obese.", bmi);
+        printf("Your BMI is %.2f and you are obese.\n", bmi);
     }
 
     return 0;
