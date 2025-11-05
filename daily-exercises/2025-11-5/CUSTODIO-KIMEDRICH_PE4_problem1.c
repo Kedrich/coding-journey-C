3a5054123351e03803fbf1884d57cb5d492b82d7
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-05 16:28:55 +0800

    Problem 1

---
+++ daily-exercises/2025-11-5/CUSTODIO-KIMEDRICH_PE4_problem1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int side1, side2, side3;
+    printf("Enter side 1: "); 
+    scanf("%d", &side1);
+    printf("Enter side 2: ");
+    scanf("%d", &side2);
+    printf("Enter side 3: ");
+    scanf("%d", &side3);
+
+    if (side1 < 0 || side2 < 0 || side3 < 0)
+    {
+        printf("Invalid sides.\n");
+        return 0;
+    }
+
+    if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+    {
+        printf("Not a valid triangle.\n");
+        return 0;
+    }
+
+    if (side1 == side2 && side2 == side3)
+    {
+        printf("Triangle type: Equilateral");
+    }
+    else if (side1 == side2 || side2 == side3 || side1 == side3)
+    {
+        printf("Triangle type: Isosceles");
+    }
+    else
+    {
+        printf("Triangle type: Scalene");
+    }
+
+
+    return 0;
+}
