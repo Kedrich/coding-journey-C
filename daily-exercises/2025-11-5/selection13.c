bc361760b0a119138574fd54f219b3eae3460438
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-05 15:13:40 +0800

    Selection 13

--- daily-exercises/2025-11-5/selection13.c
+++ daily-exercises/2025-11-5/selection13.c
@@ -3,8 +3,22 @@
 int main(void)
 {
 
-    
+    char letter;
+    printf("Please enter a letter (a-z or A-Z): ");
+    scanf(" %c", &letter);
 
+    if(letter >= 'a' && letter <= 'z')
+    {
+        printf("The letter %c is lowercase.", letter);
+    }
+    else if (letter >= 'A' && letter <= 'Z')
+    {
+        printf("The letter %c is uppercase.", letter);
+    }
+    else
+    {
+        printf("Please enter a valid letter.");
+    }
 
     return 0;
 }
---
+++ daily-exercises/2025-11-5/selection16.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int n;
+
+    return 0;
+}
