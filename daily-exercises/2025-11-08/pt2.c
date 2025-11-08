a2f75b7b8de9b55b5939037a07e639dee4301ad4
Kim Edrich B. Custodio <kedrich.custodio@gmail.com>
2025-11-08 19:05:07 +0800

    Sum of Arrays using pointers

---
+++ daily-exercises/2025-11-08/pt2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n], *p, i;
+    for(p = &arr[0], i = 1;p < &arr[n]; p++,  i++)
+    {
+        printf("Enter element %d: ", i);
+        scanf("%d", p);
+    }
+
+    int sum  = 0;
+    for(p = &arr[0]; p < &arr[n]; p++)
+    {
+        sum += *p;
+    }
+
+    printf("The sum of the %d array is: %d", n, sum);
+
+    return 0;
+}
